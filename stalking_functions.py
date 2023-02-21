f3c8e4e84159f73c02d021dbbe46abace827e9a2
Stardecoder <nedjmeddine.allab@gmail.com>
2023-02-21 21:53:19 +0100

    WIP: + basic profile stalking function

--- stalking_functions.py
+++ stalking_functions.py
@@ -4,27 +4,13 @@ Created on Thu Feb 16 20:17:45 2023
 
 @author: StarDecoder
 """
-
-
-from utils_functions import read_params, connect_to_my_ig_account, map_hashtags_with_medias, collapse_hashtags_media_dict 
-from auto_browsing import generate_daily_session_params,  basic_daily_auto_browsing
-
+from datetime import datetime
+import os
+import json
 import logging
 logging.basicConfig(level = logging.INFO)
 
 my_log = logging.getLogger("instabot_2023")
-
-
-PARAMS_FILENAME = "params.yaml"
-params = read_params(PARAMS_FILENAME)
-
-
-cl = connect_to_my_ig_account(params)
-
-
-#user_info = cl.user_id_from_username("avicii")
-
-#cl.user_info_by_username('adw0rd').dict()
 
 
 def download_medias(cl, medias, store_at = "data_instabot_2023"):
@@ -32,3 +18,33 @@ def download_medias(cl, medias, store_at = "data_instabot_2023"):
     for m in medias:
         if m.media_type == 1:
             cl.photo_download(m.pk, store_at)
+
+
+def stalk_profile(cl, username, data_folder = "data_instabot_2023"):
+    '''
+    for given username the function creates local folder with profile picture
+    and relevant informations (name, bio, is_private,....) in json file named profile_infos.json
+    '''
+    try:
+        user_info = cl.user_info_by_username(username).dict()
+     
+        profile_dir = os.path.join(data_folder, "profiles", user_info['username'])
+        profile_pic_path = os.path.join(profile_dir, user_info['username'] + "_profile_pic_" + str(datetime.now()).replace(':','-').replace('.','-'))
+     
+        if not os.path.exists(profile_dir):
+            os.makedirs(profile_dir)      
+            
+        cl.photo_download_by_url(user_info['profile_pic_url_hd'], profile_pic_path)
+        
+        with open(os.path.join(profile_dir,'profile_infos.json'), 'w') as f:
+            json.dump(user_info, f)
+    except Exception as e:
+        my_log.exception(e)
+
+
+
+
+
+
+
+
